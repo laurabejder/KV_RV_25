c64a20107db16c96f123c51bc414ca7e86e87c55
laurabejder <laurabejder@gmail.com>
2025-10-02 13:45:19 +0200

    added a bunch of things

---
+++ 01_hent_data.py
@@ -0,0 +1,43 @@
+import paramiko
+import os
+
+# FTP login information
+host = "data.valg.dk"
+port = 22
+username = "Valg"
+password = "Valg"
+
+# Forbindelse til FTP-server
+transport = paramiko.Transport((host, port))
+transport.connect(username=username, password=password)
+sftp = paramiko.SFTPClient.from_transport(transport)
+
+# Funktioner til download af filer og mapper
+def download_files(sftp, remote_dir, local_dir, folder_name):
+    files = sftp.listdir(remote_dir+"/"+folder_name)
+
+    for file in files:   # download each file
+        remote_file_path = remote_dir + "/" + folder_name + "/" + file
+        local_file_path = os.path.join(local_dir, folder_name, file)
+        os.makedirs(os.path.dirname(local_file_path), exist_ok=True)
+        sftp.get(remote_file_path, local_file_path)
+
+def download_folders(folders):
+    for folder in folders:
+        download_files(sftp, remote_path, local_path, folder)
+
+# Download KV25 data
+remote_path = "/data/kommunalvalg-134-18-11-2025/verifikation"
+local_path = "data/raw/kv"
+folders = ["valgresultater", "kandidat-data", "mandatfordeling", "valgdeltagelse"]
+download_folders(folders)
+
+# Download RV25 data
+remote_path = "/data/regionsrådsvalg-134-18-11-2025/verifikation"
+local_path = "data/raw/rv"
+folders = ["valgresultater", "mandatfordeling", "valgdeltagelse"]
+download_folders(folders)
+
+# Luk forbindelsen
+sftp.close()
+transport.close()
