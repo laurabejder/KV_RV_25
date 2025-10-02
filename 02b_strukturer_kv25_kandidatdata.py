1e14f07098bd1da2d3fa9efb14a03e2cc3dd9437
laurabejder <laurabejder@gmail.com>
2025-10-02 15:18:53 +0200

    added valgforbund data

--- 02b_strukturer_kv25_kandidatdata.py
+++ 02b_strukturer_kv25_kandidatdata.py
@@ -12,10 +12,25 @@ to_path = "data/struktureret/"
 
 def get_kv_kandidatdata(from_path, to_path, valg, data_type):
     all_files = kombiner_resultater(from_path, to_path, "kv", "kandidat-data")
+    valgforbund_data = []
     kandidat_data = []
 
     for file in all_files:
         data = json.load(open(file, 'r', encoding='utf-8'))
+        try:
+        
+            for valgforbund in data['Valgforbund']:
+                valgforbund_data.append({
+                    'kommune': data['Kommune'],
+                    'kommune_dagi_id': data['KommuneDagiId'],
+                    'frigivelsestidspunkt': data['FrigivelsesTidspunktUTC'],
+                    'opdateringstidspunkt': data['OpdateringsTidspunktUTC'],
+                    'valgforbund_navn': valgforbund['Navn'],
+                    'kandidatliste_id': valgforbund['KandidatlisteId']
+                })
+        except Exception as e:
+            print(f"Fejl ved læsning af valgforbund i {file}: {e}")
+
         try:
             for kandidater in data['Kandidatlister']:
                 for kandidat in kandidater['Kandidater']:
@@ -28,6 +43,7 @@ def get_kv_kandidatdata(from_path, to_path, valg, data_type):
                         'parti_navn': kandidater['Navn'],
                         'parti_bogstav': kandidater['Bogstavbetegnelse'],
                         'parti_opstillingsform': kandidater['Opstillingsform'],
+                        'kandidatliste_id': kandidater['KandidatlisteId'],
                         'kandidat_navn': kandidat['Navn'],
                         'kandidat_stemmeseddelnavn': kandidat['Stemmeseddelnavn'],
                         'kandidat_stilling': kandidat['Stilling']
@@ -35,12 +51,23 @@ def get_kv_kandidatdata(from_path, to_path, valg, data_type):
         except Exception as e:  
             print(f"Fejl ved læsning af {file}: {e}")
     
-    return kandidat_data
+    return kandidat_data, valgforbund_data
 
-kandidat_data = get_kv_kandidatdata(from_path, to_path, "kv", "kandidat-data")
-df_kandidat_data = pd.DataFrame(kandidat_data)
+data = get_kv_kandidatdata(from_path, to_path, "kv", "kandidat-data")
+df_kandidat_data = pd.DataFrame(data[0])
+df_valgforbund_data = pd.DataFrame(data[1])
 
-df_kandidat_data['opdateringstidspunkt'] = pd.to_datetime(df_kandidat_data['opdateringstidspunkt'], format='%d-%m-%Y %H:%M:%S')
-df_kandidat_data['frigivelsestidspunkt'] = pd.to_datetime(df_kandidat_data['frigivelsestidspunkt'], format='%d-%m-%Y %H:%M:%S')
+def convert_to_datetime(date_str):
+    try:
+        return datetime.datetime.strptime(date_str, '%d-%m-%Y %H:%M:%S')
+    except ValueError:
+        return pd.NaT
+
+
+df_kandidat_data['opdateringstidspunkt'] = df_kandidat_data['opdateringstidspunkt'].apply(convert_to_datetime)
+df_kandidat_data['frigivelsestidspunkt'] = df_kandidat_data['frigivelsestidspunkt'].apply(convert_to_datetime)
+df_valgforbund_data['opdateringstidspunkt'] = df_valgforbund_data['opdateringstidspunkt'].apply(convert_to_datetime)
+df_valgforbund_data['frigivelsestidspunkt'] = df_valgforbund_data['frigivelsestidspunkt'].apply(convert_to_datetime)
 
 df_kandidat_data.to_csv("data/struktureret/kv25_kandidat_data.csv", index=False)
+df_valgforbund_data.to_csv("data/struktureret/kv25_valgforbund_data.csv", index=False)
