f915aec4c75fd23004f096fe901b90838a0de459
laurabejderjensen <lj2569@columbia.edu>
2025-11-06 13:10:26 +0100

    renamed files

--- data/shapes/generate_shapes.py
+++ data/shapes/generate_shapes.py
@@ -49,3 +49,44 @@ for (kommune_id, kommunenavn_slug), feats in groups.items():
     topojson_path = os.path.join(out_dir, f"{base_name}.topojson")
     with open(topojson_path, "w", encoding="utf-8") as f:
         json.dump(topo.to_dict(), f, ensure_ascii=False, indent=2)
+
+
+
+
+
+
+### Do the same for regioner
+
+for feature in afstemningsområder["features"]:
+    props = feature.get("properties", {})
+    region_id_raw = props.get("regionskode")
+    regionsnavn = props.get("regionsnavn", "")
+    if not region_id_raw or not regionsnavn:
+        continue
+
+    if regionsnavn in ['Region Hovedstaden', 'Region Sjælland']:
+        regionsnavn = "Region Østdanmark"
+        region_id_raw = "1085"  # FIND DET RIGTIGE ID
+
+    # --- remove leading zeros ---
+    region_id = str(int(region_id_raw))
+
+    regionsnavn_slug = slugify(regionsnavn)
+    groups[(region_id, regionsnavn_slug)].append(feature)
+
+# write one file per kommune (GeoJSON + TopoJSON)
+for (region_id, regionsnavn_slug), feats in groups.items():
+    fc = {"type": "FeatureCollection", "features": feats}
+    base_name = f"{region_id}_{regionsnavn_slug}_afstemningsomraader"
+
+    # save GeoJSON
+    geojson_path = os.path.join(out_dir, f"{base_name}.geojson")
+    print(f"Saving {region_id} - {regionsnavn_slug} ({len(feats)} features)")
+    with open(geojson_path, "w", encoding="utf-8") as f:
+        json.dump(fc, f, ensure_ascii=False, indent=2)
+
+    # save TopoJSON
+    topo = Topology(fc)
+    topojson_path = os.path.join(out_dir, f"{base_name}.topojson")
+    with open(topojson_path, "w", encoding="utf-8") as f:
+        json.dump(topo.to_dict(), f, ensure_ascii=False, indent=2)
