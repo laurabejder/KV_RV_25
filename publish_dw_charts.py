e2a777852c63011019d3c4d7493d2cf7d89ff821
laurabejder <laurabejder@gmail.com>
2025-11-13 10:53:29 +0100

    added dw publish code

---
+++ publish_dw_charts.py
@@ -0,0 +1,31 @@
+import requests
+import json
+
+from generate_dw_charts import DW_TOKEN
+
+dw_charts = json.load(open("dw_charts.json", "r", encoding="utf-8"))
+
+
+print("Publishing charts...")
+
+print(dw_charts)
+
+for chart in dw_charts.values():
+    for values in chart.values():
+        if isinstance(values, dict) and "id" in values:
+            print(f"Publishing chart with id {values['id']}")
+            chart_id = values['id']
+
+
+            url = f"https://api.datawrapper.de/v3/charts/{chart_id}/publish"
+
+            payload = { "callWebhooks": True }
+            headers = {
+                "Authorization": f"Bearer {DW_TOKEN}",
+                "accept": "*/*",
+                "content-type": "application/json"
+            }
+
+            response = requests.post(url, json=payload, headers=headers)
+
+            print(response.text)
