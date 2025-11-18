88cab0a86b594380ebd96c90877d777570a4e104
laurabejderjensen <lj2569@columbia.edu>
2025-11-18 23:45:54 +0100

    jaja

---
+++ publish_dw_charts.py
@@ -0,0 +1,34 @@
+# import requests
+# import json
+# import os
+# from dotenv import load_dotenv
+
+# if DW_TOKEN is None:
+#     raise ValueError("DW_TOKEN not found in .env file")
+
+# dw_charts = json.load(open("dw_charts.json", "r", encoding="utf-8"))
+
+
+# print("Publishing charts...")
+
+# print(dw_charts)
+
+# for chart in dw_charts.values():
+#     for values in chart.values():
+#         if isinstance(values, dict) and "id" in values:
+#             print(f"Publishing chart with id {values['id']}")
+#             chart_id = values['id']
+
+
+#             url = f"https://api.datawrapper.de/v3/charts/{chart_id}/publish"
+
+#             payload = { "callWebhooks": True }
+#             headers = {
+#                 "Authorization": f"Bearer {DW_TOKEN}",
+#                 "accept": "*/*",
+#                 "content-type": "application/json"
+#             }
+
+#             response = requests.post(url, json=payload, headers=headers)
+
+#             print(response.text)
