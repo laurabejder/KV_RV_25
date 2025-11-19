33788505e87ee2bf40b36fb7312d6095a9306e45
laurabejderjensen <lj2569@columbia.edu>
2025-11-19 17:10:51 +0100

    fixed bug

--- 05a_opdater_kv_datafiler.py
+++ 05a_opdater_kv_datafiler.py
@@ -28,6 +28,11 @@ kv25_resultater_partier = (
     .reset_index(drop=True)
 )
 
+kv25_resultater_kandidater["kommune"] = kv25_resultater_kandidater["kommune"].str.replace(" Kommune", "", regex=False)
+kv25_resultater_kandidater["kommune"] = kv25_resultater_kandidater["kommune"].str.replace("s Regionskommune", "", regex=False)
+kv25_resultater_kandidater["kommune"] = kv25_resultater_kandidater["kommune"].str.replace("Københavns Kommune", "Københavns", regex=False)
+kv25_resultater_kandidater["kommune"] = kv25_resultater_kandidater["kommune"].str.replace("Københavns", "København", regex=False)
+
 kv25_resultater_partier["kommune"] = kv25_resultater_partier["kommune"].str.replace(" Kommune", "", regex=False)
 kv25_resultater_partier["kommune"] = kv25_resultater_partier["kommune"].str.replace("s Regionskommune", "", regex=False)
 kv25_resultater_partier["kommune"] = kv25_resultater_partier["kommune"].str.replace("Københavns Kommune", "Københavns", regex=False)
